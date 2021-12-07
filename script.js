bae98485803dd7deb36d3c954b213743ff658058
Diasbauyr <91119416+Diasbauyr@users.noreply.github.com>
2021-12-07 14:23:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('.linesub').click(function(){
+    $('.main-header__subjects').animate({"height"  : 'toggle'}, 'slow' );
+    $(".main-header__courses").css("display", "none");
+    $(".main-header__future").css("display", "none");
+    $(this).next('.icon').toggleClass('active');
+    });
+
+  $('.linecourse').click(function(){
+    $('.main-header__courses').animate({"height"  : 'toggle'}, 'slow' );
+
+    $(".main-header__subjects").css("display", "none");
+
+    $(".main-header__future").css("display", "none");
+  });
+
+  $('.linefut').click(function(){
+    $('.main-header__future').animate({"height"  : 'toggle'}, 'slow' );
+    
+    $(".main-header__subjects").css("display", "none");
+
+    $(".main-header__courses").css("display", "none");
+  });
+});
+ 
